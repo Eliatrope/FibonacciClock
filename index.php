ada8ebd538357679a7e33556ad3774f528f124d3
BALLA <hugo.ballabriga@laposte.net>
2018-01-04 10:06:17 +0000

    Replace bg-color by class...
    Better readibility

--- index.php
+++ index.php
@@ -11,31 +11,31 @@
   $month = date("m");
   $year = date("y");
 
-  $fiboh5 = "rgb(93, 95, 99)";
-  $fiboh3 = "rgb(93, 95, 99)";
-  $fiboh2 = "rgb(93, 95, 99)";
-  $fiboh1 = "rgb(93, 95, 99)";
-  $fiboh1_0 = "rgb(93, 95, 99)";
+  $fiboh5 = "black";
+  $fiboh3 = "black";
+  $fiboh2 = "black";
+  $fiboh1 = "black";
+  $fiboh1_0 = "black";
 
-  $fibom5 = "rgb(93, 95, 99)";
-  $fibom3 = "rgb(93, 95, 99)";
-  $fibom2 = "rgb(93, 95, 99)";
-  $fibom1 = "rgb(93, 95, 99)";
-  $fibom1_0 = "rgb(93, 95, 99)";
+  $fibom5 = "black";
+  $fibom3 = "black";
+  $fibom2 = "black";
+  $fibom1 = "black";
+  $fibom1_0 = "black";
 
-  /*$fibo5 = "rgb(93, 95, 99)";
-  $fibo3 = "rgb(93, 95, 99)";
-  $fibo2 = "rgb(93, 95, 99)";
-  $fibo1 = "rgb(93, 95, 99)";
-  $fibo1_0 = "rgb(93, 95, 99)";*/
+  /*$fibo5 = "black";
+  $fibo3 = "black";
+  $fibo2 = "black";
+  $fibo1 = "black";
+  $fibo1_0 = "black";*/
 
   $approxMinute = $minute * 5;
   echo 'Il est à peu près '.$hour.':'.$minute.'(*5 -> ~'.$approxMinute.')'.$amorpm;
 
-  //red rgb(235,87,103) is for hour
-  //blue rgb(100,103,244) is for hour and minute
-  //green rgb(90,232,124) is for minute
-  //black rgb(93, 95, 99) is for void (or 0... 0 != void)
+  //red is for hour
+  //blue is for hour and minute
+  //green is for minute
+  //black is for void (or 0... 0 != void)
 
 
   if($amorpm == "pm" && $hour == 12){//midnight
@@ -44,214 +44,214 @@
 
   switch($hour){
     case 0 :
-      $fiboh5 = "rgb(93, 95, 99)";
-      $fiboh3 = "rgb(93, 95, 99)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "black";
+      $fiboh3 = "black";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 1 :
-      $fiboh5 = "rgb(93, 95, 99)";
-      $fiboh3 = "rgb(93, 95, 99)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(235,87,103)";
+      $fiboh5 = "black";
+      $fiboh3 = "black";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "red";
     break;
     case 2 :
-      $fiboh5 = "rgb(93, 95, 99)";
-      $fiboh3 = "rgb(93, 95, 99)";
-      $fiboh2 = "rgb(235,87,103)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "black";
+      $fiboh3 = "black";
+      $fiboh2 = "red";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 3 :
-      $fiboh5 = "rgb(93, 95, 99)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "black";
+      $fiboh3 = "red";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 4 :
-      $fiboh5 = "rgb(93, 95, 99)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(235,87,103)";
+      $fiboh5 = "black";
+      $fiboh3 = "red";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "red";
     break;
     case 5 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(93, 95, 99)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "red";
+      $fiboh3 = "black";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 6 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(93, 95, 99)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(235,87,103)";
+      $fiboh5 = "red";
+      $fiboh3 = "black";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "red";
     break;
     case 7 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(93, 95, 99)";
-      $fiboh2 = "rgb(235,87,103)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "red";
+      $fiboh3 = "black";
+      $fiboh2 = "red";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 8 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "red";
+      $fiboh3 = "red";
+      $fiboh2 = "black";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 9 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(93, 95, 99)";
-      $fiboh1 = "rgb(235,87,103)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "red";
+      $fiboh3 = "red";
+      $fiboh2 = "black";
+      $fiboh1 = "red";
+      $fiboh1_0 = "black";
     break;
     case 10 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(235,87,103)";
-      $fiboh1 = "rgb(93, 95, 99)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "red";
+      $fiboh3 = "red";
+      $fiboh2 = "red";
+      $fiboh1 = "black";
+      $fiboh1_0 = "black";
     break;
     case 11 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(235,87,103)";
-      $fiboh1 = "rgb(235,87,103)";
-      $fiboh1_0 = "rgb(93, 95, 99)";
+      $fiboh5 = "red";
+      $fiboh3 = "red";
+      $fiboh2 = "red";
+      $fiboh1 = "red";
+      $fiboh1_0 = "black";
     break;
     case 12 :
-      $fiboh5 = "rgb(235,87,103)";
-      $fiboh3 = "rgb(235,87,103)";
-      $fiboh2 = "rgb(235,87,103)";
-      $fiboh1 = "rgb(235,87,103)";
-      $fiboh1_0 = "rgb(235,87,103)";
+      $fiboh5 = "red";
+      $fiboh3 = "red";
+      $fiboh2 = "red";
+      $fiboh1 = "red";
+      $fiboh1_0 = "red";
     break;
   }
   switch($minute){
     case 0 :
-      $fibom5 = "rgb(93, 95, 99)";
-      $fibom3 = "rgb(93, 95, 99)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "black";
+      $fibom3 = "black";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 1 :
-      $fibom5 = "rgb(93, 95, 99)";
-      $fibom3 = "rgb(93, 95, 99)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(90,232,124)";
+      $fibom5 = "black";
+      $fibom3 = "black";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "green";
     break;
     case 2 :
-      $fibom5 = "rgb(93, 95, 99)";
-      $fibom3 = "rgb(93, 95, 99)";
-      $fibom2 = "rgb(90,232,124)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "black";
+      $fibom3 = "black";
+      $fibom2 = "green";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 3 :
-      $fibom5 = "rgb(93, 95, 99)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "black";
+      $fibom3 = "green";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 4 :
-      $fibom5 = "rgb(93, 95, 99)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(90,232,124)";
+      $fibom5 = "black";
+      $fibom3 = "green";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "green";
     break;
     case 5 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(93, 95, 99)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "green";
+      $fibom3 = "black";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 6 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(93, 95, 99)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(90,232,124)";
+      $fibom5 = "green";
+      $fibom3 = "black";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "green";
     break;
     case 7 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(93, 95, 99)";
-      $fibom2 = "rgb(90,232,124)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "green";
+      $fibom3 = "black";
+      $fibom2 = "green";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 8 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "green";
+      $fibom3 = "green";
+      $fibom2 = "black";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 9 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(93, 95, 99)";
-      $fibom1 = "rgb(90,232,124)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "green";
+      $fibom3 = "green";
+      $fibom2 = "black";
+      $fibom1 = "green";
+      $fibom1_0 = "black";
     break;
     case 10 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(90,232,124)";
-      $fibom1 = "rgb(93, 95, 99)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "green";
+      $fibom3 = "green";
+      $fibom2 = "green";
+      $fibom1 = "black";
+      $fibom1_0 = "black";
     break;
     case 11 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(90,232,124)";
-      $fibom1 = "rgb(90,232,124)";
-      $fibom1_0 = "rgb(93, 95, 99)";
+      $fibom5 = "green";
+      $fibom3 = "green";
+      $fibom2 = "green";
+      $fibom1 = "green";
+      $fibom1_0 = "black";
     break;
     case 12 :
-      $fibom5 = "rgb(90,232,124)";
-      $fibom3 = "rgb(90,232,124)";
-      $fibom2 = "rgb(90,232,124)";
-      $fibom1 = "rgb(90,232,124)";
-      $fibom1_0 = "rgb(90,232,124)";
+      $fibom5 = "green";
+      $fibom3 = "green";
+      $fibom2 = "green";
+      $fibom1 = "green";
+      $fibom1_0 = "green";
     break;
   }
-  if($fiboh5 == "rgb(235,87,103)" && $fibom5 == "rgb(90,232,124)"){
-    $fibo5 = "rgb(100,103,244)";
-    $fiboh5 = "rgb(100,103,244)";
-    $fibom5 = "rgb(100,103,244)";
+  if($fiboh5 == "red" && $fibom5 == "green"){
+    $fibo5 = "blue";
+    $fiboh5 = "blue";
+    $fibom5 = "blue";
   }
-  if($fiboh3 == "rgb(235,87,103)" && $fibom3 == "rgb(90,232,124)"){
-    $fibo3 = "rgb(100,103,244)";
-    $fiboh3 = "rgb(100,103,244)";
-    $fibom3 = "rgb(100,103,244)";
+  if($fiboh3 == "red" && $fibom3 == "green"){
+    $fibo3 = "blue";
+    $fiboh3 = "blue";
+    $fibom3 = "blue";
   }
-  if($fiboh2 == "rgb(235,87,103)" && $fibom2 == "rgb(90,232,124)"){
-    $fibo2 = "rgb(100,103,244)";
-    $fiboh2 = "rgb(100,103,244)";
-    $fibom2 = "rgb(100,103,244)";
+  if($fiboh2 == "red" && $fibom2 == "green"){
+    $fibo2 = "blue";
+    $fiboh2 = "blue";
+    $fibom2 = "blue";
   }
-  if($fiboh1 == "rgb(235,87,103)" && $fibom1 == "rgb(90,232,124)"){
-    $fibo1 = "rgb(100,103,244)";
-    $fiboh1 = "rgb(100,103,244)";
-    $fibom1 = "rgb(100,103,244)";
+  if($fiboh1 == "red" && $fibom1 == "green"){
+    $fibo1 = "blue";
+    $fiboh1 = "blue";
+    $fibom1 = "blue";
   }
-  if($fiboh1_0 == "rgb(235,87,103)" && $fibom1_0 == "rgb(90,232,124)"){
-    $fibo1_0 = "rgb(100,103,244)";
-    $fiboh1_0 = "rgb(100,103,244)";
-    $fibom1_0 = "rgb(100,103,244)";
+  if($fiboh1_0 == "red" && $fibom1_0 == "green"){
+    $fibo1_0 = "blue";
+    $fiboh1_0 = "blue";
+    $fibom1_0 = "blue";
   }
 
 
@@ -264,11 +264,11 @@
   <body>
     <section class="main_container">
       <div class="container_clock_fibonacci">
-        <div class="fibonacci5" style="background-color:
+        <div class="fibonacci5 
         <?php
-          if($fiboh5 == "rgb(93, 95, 99)"){
+          if($fiboh5 == "black"){
             echo $fibom5;
-          }elseif($fibom5 == "rgb(93, 95, 99)"){
+          }elseif($fibom5 == "black"){
             echo $fiboh5;}
           else{
             echo $fibo5;
@@ -277,11 +277,11 @@
         ">
           <?php echo $year;?>
         </div>
-        <div class="fibonacci3" style="background-color:
+        <div class="fibonacci3
         <?php
-          if($fiboh3 == "rgb(93, 95, 99)"){
+          if($fiboh3 == "black"){
             echo $fibom3;
-          }elseif($fibom3 == "rgb(93, 95, 99)"){
+          }elseif($fibom3 == "black"){
             echo $fiboh3;
           }else{
             echo $fibo3;
@@ -289,11 +289,11 @@
         ?>">
           <?php echo $day;?>
         </div>
-        <div class="fibonacci2" style="background-color:
+        <div class="fibonacci2
         <?php
-          if($fiboh2 == "rgb(93, 95, 99)"){
+          if($fiboh2 == "black"){
             echo $fibom2;
-          }elseif($fibom2 == "rgb(93, 95, 99)"){
+          }elseif($fibom2 == "black"){
             echo $fiboh2;
           }else{
             echo $fibo2;
@@ -302,11 +302,11 @@
         ">
           <?php echo $month;?>
         </div>
-        <div class="fibonacci1" style="background-color:
+        <div class="fibonacci1
         <?php
-          if($fiboh1 == "rgb(93, 95, 99)"){
+          if($fiboh1 == "black"){
             echo $fibom1;
-          }elseif($fibom1 == "rgb(93, 95, 99)"){
+          }elseif($fibom1 == "black"){
             echo $fiboh1;
           }else{
             echo $fibo1;
@@ -314,11 +314,11 @@
         ?>
         ">
         </div>
-        <div class="fibonacci1_0" style="background-color:
+        <div class="fibonacci1_0
         <?php
-          if($fiboh1_0 == "rgb(93, 95, 99)"){
+          if($fiboh1_0 == "black"){
             echo $fibom1_0;
-          }elseif($fibom1_0 == "rgb(93, 95, 99)"){
+          }elseif($fibom1_0 == "black"){
             echo $fiboh1_0;
           }else{
             echo $fibo1_0;
